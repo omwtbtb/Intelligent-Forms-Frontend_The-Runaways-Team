dc91da0e05ab3dc57d7eb7d6b9f76ef3f5d1a232
PaulSmocot <110483174+PaulSmocot@users.noreply.github.com>
2023-02-28 18:36:16 +0200

    End-Point Testing

--- Intelligent Forms/src/App.jsx
+++ Intelligent Forms/src/App.jsx
@@ -1,11 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [tests, SetTest] = useState([]);
 
-  return <div className="App">Test</div>;
+  useEffect(() => {
+    axios
+      .get("https://intelligentformsapi.azurewebsites.net/test")
+      .then((response) => {
+        SetTest(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div>
+      {tests.id} , {tests._self}
+    </div>
+  );
 }
 
 export default App;

ad823670220bad537d2bc8a0b94272f9bba21c86
PaulSmocot <smocotpaul@yahoo.com>
2023-03-01 12:23:32 +0200

    BackGround+Title

---
+++ Intelligent-Forms/src/App.jsx
@@ -0,0 +1,9 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+
+function App() {
+  return <div className="App">Test</div>;
+}
+
+export default App;

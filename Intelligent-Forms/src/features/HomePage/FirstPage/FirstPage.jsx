802e6ac89b62d284e418cefe1ba820859f9d39f3
Smocot Paul Emanuel <110483174+PaulSmocot@users.noreply.github.com>
2023-03-02 15:06:47 +0200

    Feature/implement project arhitecture (#3)
    * BackGround+Title
    
    * feature/implement-home-page
    
    * Add assets folder
    
    * feature/implement-project-arhitecture

--- Intelligent-Forms/src/features/HomePage/FirstPage/FirstPage.jsx
+++ Intelligent-Forms/src/features/HomePage/FirstPage/FirstPage.jsx
@@ -2,28 +2,12 @@ import React from "react";
 import "./FirstPage.css";
 import { Menubar } from "primereact/menubar";
 import { Link } from "react-router-dom";
+import NavBar from "./NavBar";
 
 function FirstPage() {
   return (
     <div className="FirstPage">
-      <div className="Nav-Bar">
-        <div className="Form">
-          <img src="assets/Logo.png" alt="Logo" />
-          <div className="Inf1">Intelligent Forms</div>
-        </div>
-        <Link to="/" className="Home">
-          Home
-        </Link>
-        <Link to="/Contact_Us" className="Contact-Us">
-          Contact Us
-        </Link>
-        <div className="Login_Register">
-          <img src="assets/Login.png" alt="Logo" />
-          <Link to="/Login_Register" className="Inf2">
-            Login/Register
-          </Link>
-        </div>
-      </div>
+      <NavBar />
       <div className="Poza">
         <img src="assets/Form.png" alt="Logo" />
       </div>
---
+++ Intelligent-Forms/src/features/HomePage/FirstPage/NavBar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./FirstPage.css";
+import { Link } from "react-router-dom";
+
+function NavBar() {
+  return (
+    <div className="Nav-Bar">
+      <div className="Form">
+        <img src="assets/Logo.png" alt="Logo" />
+        <div className="Inf1">Intelligent Forms</div>
+      </div>
+      <Link to="/" className="Home">
+        Home
+      </Link>
+      <Link to="/Contact_Us" className="Contact-Us">
+        Contact Us
+      </Link>
+      <div className="Login_Register">
+        <img src="assets/Login.png" alt="Logo" />
+        <Link to="/Login_Register" className="Inf2">
+          Login/Register
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;

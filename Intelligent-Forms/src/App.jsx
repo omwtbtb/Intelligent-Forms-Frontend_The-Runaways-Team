d4b7dfb52eac35ad82305412e4f3406d75f34c28
PaulSmocot <smocotpaul@yahoo.com>
2023-03-02 12:35:21 +0200

    feature/implement-home-page

--- Intelligent-Forms/src/App.jsx
+++ Intelligent-Forms/src/App.jsx
@@ -1,9 +1,12 @@
-import { useState, useEffect } from "react";
 import "./App.css";
-import axios from "axios";
+import AppRouter from "./assets/HomePage/Router/Route";
 
 function App() {
-  return <div className="App">Test</div>;
+  return (
+    <div className="App">
+      <AppRouter />
+    </div>
+  );
 }
 
 export default App;
